4465968e9b38aa8ad117f7c01c5d2ba29ff8ea12
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-09-06 21:33:09 -0300

    agregando ifs

--- calculadora/script.js
+++ calculadora/script.js
@@ -4,8 +4,30 @@ const botones = document.querySelectorAll(".btn");
 botones.forEach(boton => {
     boton.addEventListener("click", () => {
         const botonApretado = boton.textContent;
+        
 
-        if(pantalla.textContent === "0") {
+        // cuando presione C vuelve a 0
+        if (boton.id === "c") {
+            pantalla.textContent = "0";
+            return;
+        }
+        // borrar ultimo numero, -1 arranca a borrar desde atras
+        if (boton.id === "borrar") {
+            if(pantalla.textContent.length === 1) {
+                pantalla.textContent = "0";
+            } else { // si tenemos un numero que vuelva a 0
+            pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            return;
+        }
+
+        if (boton.id === "igual") {  // eval evaluar lo que haya en la pantalla
+            pantalla.textContent = eval(pantalla.textContent);
+            return;
+        }
+
+        // saca el cero una vez tipee numeros
+        if (pantalla.textContent === "0") { 
             pantalla.textContent = botonApretado;
         } else {
             pantalla.textContent += botonApretado;
